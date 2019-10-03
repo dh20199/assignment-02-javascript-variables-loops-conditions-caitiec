669381cfc918d042bef82dfbeebd71bd4ef26a5d
Caitie Collins <caitiec14@gmail.com>
2019-10-03 16:56:35 -0400

    testing problem 1

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -31,11 +31,12 @@
  * @returns {string} should be exactly "Hello!"
  */
 function hello (Hello!) {
+  return Hello!;
   // if you want you can declare a string variable first
   // or in this case you can just return the string directly
   // with something like "return 'Hello!'"
 }
-return hello 
+
 
 // Problem 2
 // Improve your initial function by accepting a parameter
