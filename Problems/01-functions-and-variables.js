7cc868dfdc601d733f69dab4abd0aedb7d208587
Caitie Collins <caitiec14@gmail.com>
2019-10-10 16:37:01 -0400

    I hope whoever is seeingthis is enjoying my comments

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -146,9 +146,7 @@ function carefulSubtract (first, second) {
  * @returns {} a meessage about unknown, or the square of unknown if it is a number
  */
 function typeTester (unknown) {
-  Let s = any String;
-  Let n = any Number;
-  if String, return {(s)+ ' '+'yay'},
+  if String, return { String + ' '+'yay'},
   if Number, return function (n){ return n * n},
   if null, return "sorry, I can't do anything with a null value",
   else return "I don't know how to use that kind of variable";
